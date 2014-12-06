1e6e471a9acf4e487111cba3f2872e5342a713cd
diego <diegocastanares3005@gmail.com>
2014-12-05 20:35:30 -0400

    sala de espera incluido

--- public/javascripts/pagina.js
+++ public/javascripts/pagina.js
@@ -10,7 +10,7 @@ $(function($){
 		html="";
 		for (var i=0;i<server.length;i++)
 		{
-			html+="<table border='1'><tr><td>TITULO DE PARTIDA  </td><td>"+server[i].partida+"</td></tr>";
+			html+="<table border='1' width='300'><tr><td>TITULO DE PARTIDA  </td><td>"+server[i].partida+"</td></tr>";
 			html+="<tr><td>TIPO  </td><td>"+server[i].tipo+"</td></tr>";
 			html+="<tr><td>TIEMPO DE RESPUESTA  </td><td>"+server[i].tiempo+"</td><tr>";
 			html+="<tr><td colspan='2'><input type='submit' value='jugar'></td></tr></table>";
