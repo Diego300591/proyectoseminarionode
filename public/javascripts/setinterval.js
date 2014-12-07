2f8afe9a6ccaf621518669e195985a72b1c46d92
diego <diegocastanares3005@gmail.com>
2014-12-06 22:44:47 -0400

    set interval incluido

---
+++ public/javascripts/setinterval.js
@@ -0,0 +1,15 @@
+var contador=10;
+
+function restar(){
+	contador=contador-1;
+	if(contador==0){
+		acabarCuentaAtras();
+	}
+}
+
+function acabarCuentaAtras(){
+	clearInterval(temporizador);//paramos la ejecucion del metodo, indicando la variable creada al final
+	alert("¡TIEMPO!");
+}
+
+var temporizador=setInterval(function(){restar()},10000);
